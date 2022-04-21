bc136be76221ed0b1cfcffca8bcab0b57a585739
Official.abhishekpanda <64636317+abhishekpandaOfficial@users.noreply.github.com>
2022-04-22 00:42:17 +0530

    Of Operator Example

--- src/app/rxjs-learning/rxjs-learning.component.ts
+++ src/app/rxjs-learning/rxjs-learning.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-rxjs-learning',
@@ -11,9 +11,43 @@ export class RxjsLearningComponent implements OnInit {
   Orderstat!: Observable<string>;
   orderName!:string ;
 
+  /* ------------------ Of () Operator -----------*/
+
+  studentLists = ['Abhishek','Sumit','Sinu','Mama'];
+  student$ = of(this.studentLists);
+
+  studentObj = of(
+    {Id:1,Name:'Abhishek Panda',Gender:'Male'},
+    {Id:2,Name:'Akshita Panda',Gender:'Female'},
+    {Id:3,Name:'Janaki Lata Panda',Gender:'Female'},
+  )
+
+  OrderStatus =['Order-InProgress','Order-Placed','Order-Delivered','Feedback']
+
+  orderDetails = of(this.OrderStatus);
+
   constructor() { }
 
   ngOnInit(): void {
+// **************************************** Need to Subscribe the for the Observable ***********************
+this.orderDetails.subscribe(data=>
+  {
+    setInterval(()=>{
+     console.log(data);
+    },10000)
+  })
+
+
+this.student$.subscribe(data=>
+  {
+    console.log(data);
+  })
+  this.studentObj.subscribe(data=>
+    {
+      console.log(data);
+    })
+  
+
 
     this.Orderstat = new Observable(
       function(observer)
@@ -23,8 +57,6 @@ export class RxjsLearningComponent implements OnInit {
          
             observer.next('InProcess');
          
-         
-
           setInterval(()=>
           {
             observer.next('Processed');
